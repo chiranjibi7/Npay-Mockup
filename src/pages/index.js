a70301df5b40868a4aeab4fc379ba98233bb6f2b
Chiranjibi Karki <ckarki70@gmail.com>
2023-10-01 11:15:22 -0400

    visa direct withdrawal page complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import WithdrawalMethod from "./WithdrawalMethod/WithdrawalMethod";
 import CardFieldInformation from "./CardFieldInformation/CardFieldInformation";
 import CardExpiryInformation from "./CardExpiryInformation/CardExpiryInformation";
+import VisadirectWithdrawal from "./VisadirectWithdrawal/VisadirectWithdrawal";
 
-export {WithdrawalMethod,CardFieldInformation,CardExpiryInformation}
+export {WithdrawalMethod,CardFieldInformation,CardExpiryInformation, VisadirectWithdrawal}
